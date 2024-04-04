f0dbf9d2d0ac347d41f4690548ab5e07ea822c28
RuslanG13 <ruslan.gudenko13@ukr.net>
2024-04-04 23:03:06 +0300

    Add locators for inventory page

--- locators/locators_saucedemo.py
+++ locators/locators_saucedemo.py
@@ -6,3 +6,15 @@ class LoginPageLocators:
 
     ERROR_MESSAGE_CONTAINER = ("xpath", "//div[contains(@class, 'error-message-container')]")
     ERROR_LOGIN_MESSAGE = ("xpath", "//h3[@data-test='error']")
+
+
+class InventoryPageLocators:
+    ADD_TO_CART_BUTTON = ("xpath", "//button[@id='add-to-cart-sauce-labs-backpack']")
+    SHOPPING_CART_LINK = ("xpath", "//a[@data-test='shopping-cart-link']")
+
+    INVENTORY_ITEMS = ("xpath", "//div[@class='inventory_item']")
+    INVENTORY_ITEMS_NAME = ("xpath", "//div[@class='inventory_item_name']")
+
+    SHOPPING_CART_BADGE = ("xpath", "//span[@class='shopping_cart_badge']")
+
+    CART_ITEM_LABEL = ("xpath", "cart_item_label")
