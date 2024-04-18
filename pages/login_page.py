440de3a3c4c9563489576a55beb5b7a20481b48f
RuslanG13 <ruslan.gudenko13@ukr.net>
2024-04-18 05:39:01 +0300

    Add: pages/login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,13 @@
+from base_page import BasePage
+from locators.login_locators import LoginPageLocators as lpl
+from data.login_credentials import valid_login
+
+
+class LoginPage(BasePage):
+    def __init__(self, driver, url):
+        super().__init__(self, driver=driver, url=url)
+
+    def login(self):
+        self.element_is_visible(lpl.USERNAME_FIELD_LOCATOR).send_keys(valid_login["username_valid"])
+        self.element_is_visible(lpl.PASSWORD_FIELD_LOCATOR).send_keys(valid_login["password_valid"])
+        self.element_is_clickable(lpl.LOGIN_BUTTON_LOCATOR).click()
